68ac596a6564fc56f89b7d626f1b562ff481b397
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2020-07-17 21:46:06 -0400

    Add data update and data update validation scripts.

---
+++ scripts/update_data.R
@@ -0,0 +1,296 @@
+# COVID-19 Canada Open Data Working Group Data Update Script #
+# Author: Jean-Paul R. Soucy #
+
+# Download and process updated COVID-19 Canada data files from Google Sheets
+# Spreadsheet link: https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo
+
+# Note: This script assumes the working directory is set to the root directory
+# This is most easily achieved by using the provided Covid19Canada.Rproj in RStudio
+
+# load libraries
+library(dplyr) # data manipulation
+library(tidyr) # data manipulation
+
+# read update time from public recovered sheet
+update_time <- read.csv("https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=2036294689",
+                        header = FALSE, stringsAsFactors = FALSE)[1, 1] %>%
+  sub("Last update: ", "", .) %>%
+  {paste0(as.Date(sub(",*$", "", .), "%d %B %Y"), sub(".*,", "", .))}
+
+# download files from public sheets and process
+cases <- read.csv("https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=942958991",
+                  header = TRUE,
+                  stringsAsFactors = FALSE,
+                  skip = 3)
+mortality <- read.csv("https://docs.google.com/spreadsheets/u/0/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=823945927",
+                      header = TRUE,
+                      stringsAsFactors = FALSE,
+                      skip = 3)
+recovered_cum <- read.csv("https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=2036294689",
+                          header = TRUE,
+                          stringsAsFactors = FALSE,
+                          skip = 3)[, 1:3]
+testing_cum <- read.csv("https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=2106589546",
+                        header = TRUE,
+                        stringsAsFactors = FALSE,
+                        skip = 3)[, 1:4]
+codebook <- read.csv("https://docs.google.com/spreadsheets/u/1/d/1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo/export?format=csv&id=1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo&gid=0",
+                     header = TRUE,
+                     stringsAsFactors = FALSE,
+                     skip = 2)
+
+# write files and update time
+write.csv(cases, "cases.csv", row.names = FALSE)
+write.csv(mortality, "mortality.csv", row.names = FALSE)
+write.csv(recovered_cum, "recovered_cumulative.csv", row.names = FALSE)
+write.csv(testing_cum, "testing_cumulative.csv", row.names = FALSE)
+write.csv(codebook, "codebook.csv", row.names = FALSE)
+cat(paste0(update_time, "\n"), file = "update_time.txt")
+
+# create time series of cases
+
+## define provinces (alphabetical order)
+provinces_repatriated <- c("Alberta", "BC", "Manitoba", "New Brunswick", "NL", "Nova Scotia", "Nunavut", "NWT", "Ontario", "PEI", "Repatriated", "Quebec", "Saskatchewan", "Yukon")
+provinces <- c("Alberta", "BC", "Manitoba", "New Brunswick", "NL", "Nova Scotia", "Nunavut", "NWT", "Ontario", "PEI", "Quebec", "Saskatchewan", "Yukon")
+
+## convert cases to standard date format for data manipulation
+cases <- cases %>%
+  mutate(date_report = as.Date(date_report, "%d-%m-%Y"))
+
+## define min and max dates
+min_date_cases <- min(cases$date_report, na.rm = TRUE)
+max_date_cases <- max(cases$date_report, na.rm = TRUE)
+
+## create time series by province
+cases_ts <- cases %>%
+  group_by(province, date_report) %>%
+  summarise(cases = n(), .groups = "drop_last") %>%
+  right_join(
+    data.frame(
+      "province" = rep(provinces_repatriated, each = as.integer(max_date_cases - min_date_cases) + 1),
+      "date_report" = seq.Date(from = min_date_cases, to = max_date_cases, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "date_report")) %>%
+  arrange(province, date_report) %>%
+  replace_na(list(cases = 0)) %>%
+  mutate(cumulative_cases = cumsum(cases)) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_report = format(date_report, "%d-%m-%Y")
+  )
+
+## create time series by health region
+cases_ts_hr <- cases %>%
+  group_by(province, health_region, date_report) %>%
+  summarise(cases = n(), .groups = "drop_last") %>%
+  right_join(
+    data.frame(
+      slice({
+        distinct(select(., province, health_region))
+      }, rep(1:n(), each = length(seq.Date(from = min_date_cases, to = max_date_cases, by = "day")))),
+      date_report = seq.Date(from = min_date_cases, to = max_date_cases, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "health_region", "date_report")) %>%
+  arrange(province, health_region, date_report) %>%
+  replace_na(list(cases = 0)) %>%
+  mutate(cumulative_cases = cumsum(cases)) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_report = format(date_report, "%d-%m-%Y")
+  )
+
+# create time series of mortality
+
+## convert mortality to standard date format
+mortality <- mortality %>%
+  mutate(date_death_report = as.Date(date_death_report, "%d-%m-%Y"))
+
+## define min and max dates
+min_date_mortality <- min(mortality$date_death_report, na.rm = TRUE)
+max_date_mortality <- max(mortality$date_death_report, na.rm = TRUE)
+
+## create time series by province
+mortality_ts <- mortality %>%
+  group_by(province, date_death_report) %>%
+  summarise(deaths = n(), .groups = "drop_last") %>%
+  right_join(
+    data.frame(
+      "province" = rep(provinces, each = as.integer(max_date_mortality - min_date_mortality) + 1),
+      "date_death_report" = seq.Date(from = min_date_mortality, to = max_date_mortality, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "date_death_report")) %>%
+  arrange(province, date_death_report) %>%
+  replace_na(list(deaths = 0)) %>%
+  mutate(cumulative_deaths = cumsum(deaths)) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_death_report = format(date_death_report, "%d-%m-%Y")
+  )
+
+## create time series by health region
+mortality_ts_hr <- mortality %>%
+  group_by(province, health_region, date_death_report) %>%
+  summarise(deaths = n(), .groups = "drop_last") %>%
+  right_join(
+    data.frame(
+      slice({
+        distinct(select(., province, health_region))
+      }, rep(1:n(), each = length(seq.Date(from = min_date_mortality, to = max_date_mortality, by = "day")))),
+      date_death_report = seq.Date(from = min_date_mortality, to = max_date_mortality, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "health_region", "date_death_report")) %>%
+  arrange(province, health_region, date_death_report) %>%
+  replace_na(list(deaths = 0)) %>%
+  mutate(cumulative_deaths = cumsum(deaths)) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_death_report = format(date_death_report, "%d-%m-%Y")
+  )
+
+# create full time series of recovered
+
+## convert recovered to standard date format
+recovered_cum <- recovered_cum %>%
+  mutate(date_recovered = as.Date(date_recovered, "%d-%m-%Y"))
+
+## define min and max dates
+min_date_recovered <- min(recovered_cum$date_recovered, na.rm = TRUE)
+max_date_recovered <- max(recovered_cum$date_recovered, na.rm = TRUE)
+
+## create time series by province
+recovered_ts <- recovered_cum %>%
+  group_by(province, date_recovered) %>%
+  right_join(
+    data.frame(
+      "province" = rep(c(provinces, "Repatriated"), each = as.integer(max_date_recovered - min_date_recovered) + 1),
+      "date_recovered" = seq.Date(from = min_date_recovered, to = max_date_recovered, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "date_recovered")) %>%
+  arrange(province, date_recovered) %>%
+  replace_na(list(cumulative_recovered = 0)) %>%
+  ### calculate daily number of tests
+  group_by(province) %>%
+  mutate(
+    recovered = c(cumulative_recovered[1], diff(cumulative_recovered))
+  ) %>%
+  select(province, date_recovered, recovered, cumulative_recovered) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_recovered = format(date_recovered, "%d-%m-%Y")
+  )
+
+# create full time series of testing
+
+## convert testing to standard date format
+testing_cum <- testing_cum %>%
+  mutate(date_testing = as.Date(date_testing, "%d-%m-%Y"))
+
+## define min and max dates
+min_date_testing <- min(testing_cum$date_testing, na.rm = TRUE)
+max_date_testing <- max(testing_cum$date_testing, na.rm = TRUE)
+
+## create time series by province
+testing_ts <- testing_cum %>%
+  group_by(province, date_testing) %>%
+  right_join(
+    data.frame(
+      "province" = rep(provinces, each = as.integer(max_date_testing - min_date_testing) + 1),
+      "date_testing" = seq.Date(from = min_date_testing, to = max_date_testing, by = "day"),
+      stringsAsFactors = FALSE
+    ), by = c("province", "date_testing")) %>%
+  arrange(province, date_testing) %>%
+  replace_na(list(cumulative_testing = 0)) %>%
+  ### calculate daily number of tests
+  group_by(province) %>%
+  mutate(
+    testing = c(cumulative_testing[1], diff(cumulative_testing))
+  ) %>%
+  select(province, date_testing, testing, cumulative_testing, testing_info) %>%
+  ### return to non-standard date format for saving
+  mutate(
+    date_testing = format(date_testing, "%d-%m-%Y")
+  )
+
+# create time series of active cases
+
+## join cases and recovered
+active_ts <- left_join(
+  cases_ts %>% rename(date = date_report) %>% select(-cases),
+  recovered_ts %>% rename(date = date_recovered) %>% select(-recovered),
+  by = c("province", "date")
+) %>%
+  left_join(
+    mortality_ts %>% rename(date = date_death_report) %>% select(-deaths),
+    by = c("province", "date")
+  ) %>%
+  ## replace recovered = NA with 0
+  replace_na(list(cumulative_recovered = 0, cumulative_deaths = 0)) %>%
+  ## calculate active cases
+  mutate(active_cases = cumulative_cases - cumulative_recovered - cumulative_deaths) %>%
+  group_by(province) %>%
+  mutate(active_cases_change = c(active_cases[1], diff(active_cases))) %>%
+  rename(
+    date_active = date
+  )
+
+# create time series for all of Canada combined
+
+## cases
+cases_ts_canada <- cases_ts %>%
+  ungroup %>%
+  mutate(province = "Canada") %>%
+  group_by(province, date_report) %>%
+  summarise_all(sum) %>%
+  ### keep original order
+  arrange(match(date_report, cases_ts$date_report))
+
+## mortality
+mortality_ts_canada <- mortality_ts %>%
+  ungroup %>%
+  mutate(province = "Canada") %>%
+  group_by(province, date_death_report) %>%
+  summarise_all(sum) %>%
+  ### keep original order
+  arrange(match(date_death_report, mortality_ts$date_death_report))
+
+## recovered
+recovered_ts_canada <- recovered_ts %>%
+  ungroup %>%
+  mutate(province = "Canada") %>%
+  group_by(province, date_recovered) %>%
+  summarise_all(sum) %>%
+  ### keep original order
+  arrange(match(date_recovered, recovered_ts$date_recovered))
+
+## testing
+testing_ts_canada <- testing_ts %>%
+  ungroup %>%
+  mutate(province = "Canada") %>%
+  group_by(province, date_testing) %>%
+  ### if there is any testing info for any province on a particular day, it will be flagged
+  summarise(testing = sum(testing), cumulative_testing = sum(cumulative_testing), testing_info = paste0(unique(testing_info)), .groups = "drop_last") %>%
+  ### keep original order
+  arrange(match(date_testing, testing_ts$date_testing))
+
+## active cases
+active_ts_canada <- active_ts %>%
+  ungroup %>%
+  mutate(province = "Canada") %>%
+  group_by(province, date_active) %>%
+  summarise_all(sum) %>%
+  ### keep original order
+  arrange(match(date_active, active_ts$date_active))
+
+# write generated files
+write.csv(cases_ts, "timeseries_prov/cases_timeseries_prov.csv", row.names = FALSE)
+write.csv(cases_ts_hr, "timeseries_hr/cases_timeseries_hr.csv", row.names = FALSE)
+write.csv(cases_ts_canada, "timeseries_canada/cases_timeseries_canada.csv", row.names = FALSE)
+write.csv(mortality_ts, "timeseries_prov/mortality_timeseries_prov.csv", row.names = FALSE)
+write.csv(mortality_ts_hr, "timeseries_hr/mortality_timeseries_hr.csv", row.names = FALSE)
+write.csv(mortality_ts_canada, "timeseries_canada/mortality_timeseries_canada.csv", row.names = FALSE)
+write.csv(recovered_ts, "timeseries_prov/recovered_timeseries_prov.csv", row.names = FALSE)
+write.csv(recovered_ts_canada, "timeseries_canada/recovered_timeseries_canada.csv", row.names = FALSE)
+write.csv(testing_ts, "timeseries_prov/testing_timeseries_prov.csv", row.names = FALSE)
+write.csv(testing_ts_canada, "timeseries_canada/testing_timeseries_canada.csv", row.names = FALSE)
+write.csv(active_ts, "timeseries_prov/active_timeseries_prov.csv", row.names = FALSE)
+write.csv(active_ts_canada, "timeseries_canada/active_timeseries_canada.csv", row.names = FALSE)
---
+++ scripts/update_data_validation.R
@@ -0,0 +1,243 @@
+# COVID-19 Canada Open Data Working Group Data Update Validation Script #
+# Author: Jean-Paul R. Soucy #
+
+# Compare updated data files (from update_data.R) to the current version in the GitHub repository
+# GitHub repository: https://github.com/ishaberry/Covid19Canada
+
+# Note: This script assumes the working directory is set to the root directory
+# This is most easily achieved by using the provided Covid19Canada.Rproj in RStudio
+
+# load libraries
+library(dplyr) # data manipulation
+library(stringr) # manipulate strings
+library(compareDF) # compare data frames
+library(crayon) # colourful output in console
+
+# load current data from GitHub repository
+temp <- tempfile()
+tempd <- tempdir()
+download.file("https://github.com/ishaberry/Covid19Canada/archive/master.zip", temp, mode = "wb")
+unzip(temp, exdir = tempd)
+old_files <- list.files(path = tempd, pattern = "*.csv", full.names = TRUE, recursive = TRUE)
+invisible(list2env(
+  lapply(setNames(old_files, make.names(paste0("old_", sub(".csv", "", basename(old_files))))), 
+         read.csv, stringsAsFactors = FALSE), envir = .GlobalEnv))
+
+# load new data
+new_files <- list.files(path = ".", pattern = "*.csv", full.names = TRUE, recursive = TRUE)
+invisible(list2env(
+  lapply(setNames(new_files, make.names(sub(".csv", "", basename(new_files)))), 
+         read.csv, stringsAsFactors = FALSE), envir = .GlobalEnv))
+
+# load update time
+old_update_time <- readLines(paste(tempd, "Covid19Canada-master", "update_time.txt", sep = "/"))
+update_time <- readLines("update_time.txt")
+update_date <- as.Date(update_time)
+
+# stop running script if old update time and new update time are the same
+if (identical(old_update_time, update_time)) stop("Update times for old and new data are the same.")
+
+# convert all dates to ISO 8601
+for (df in names(which(unlist(eapply(.GlobalEnv, is.data.frame))))) {
+  assign(df, get(df) %>%
+           mutate_at(
+             vars(contains("date")), as.Date, format = "%d-%m-%Y")
+           )
+}
+
+# define data types
+types <- c("cases", "mortality", "recovered", "testing")
+types_hr <- c("cases", "mortality")
+
+# define function to print today's cumulative numbers
+print_cumulative_today <- function() {
+  
+  cat("Old data:", old_update_time, fill = TRUE)
+  cat("New data:", update_time, "\n", fill = TRUE)
+  cat("Today's cumulative numbers...", fill = TRUE)
+  cat("Total cases:", cases_timeseries_canada %>% filter(date_report == update_date) %>% pull(cumulative_cases), fill = TRUE)
+  cat("Cases today:", cases_timeseries_canada %>% filter(date_report == update_date) %>% pull(cases), fill = TRUE)
+  cat("Total deaths:", mortality_timeseries_canada %>% filter(date_death_report == update_date) %>% pull(cumulative_deaths), fill = TRUE)
+  cat("Deaths today:", mortality_timeseries_canada %>% filter(date_death_report == update_date) %>% pull(deaths), fill = TRUE)
+  cat("Total recovered:", recovered_timeseries_canada %>% filter(date_recovered == update_date) %>% pull(cumulative_recovered), fill = TRUE)
+  cat("Recovered today:", recovered_timeseries_canada %>% filter(date_recovered == update_date) %>% pull(recovered), fill = TRUE)
+  cat("Total testing:", testing_timeseries_canada %>% filter(date_testing == update_date) %>% pull(cumulative_testing), fill = TRUE) # number repeats?
+  cat("Testing today:", testing_timeseries_canada %>% filter(date_testing == update_date) %>% pull(testing), fill = TRUE) # number repeats?
+  
+}
+
+# define functions for individual-level data
+
+## check ages in case data
+check_ages_cases <- function() {
+  
+  cat("\nChecking ages in case data...", fill = TRUE)
+  
+  ### transform age map cases
+  age_map_cases <- setNames(age_map_cases$age_display, age_map_cases$age) # to pass to recode()
+  
+  ### transform case data
+  cases <- cases %>%
+    select(case_id, age) %>%
+    mutate(
+      age_map = factor(
+        recode(age, !!!age_map_cases),
+        c(
+          "<20",
+          "20-29",
+          "30-39",
+          "40-49",
+          "50-59",
+          "60-69",
+          "70-79",
+          "80-89",
+          "90-99",
+          "100-109",
+          "NR"
+        )
+      )
+    )
+  
+  ### check for new ages
+  if (sum(is.na(cases$age_transform)) == 0) {
+    cat(green("No new ages."), fill = TRUE)
+  } else {
+    ### report new ages
+    cat(bgRed("New ages:", paste(unique(cases[is.na(cases$age_map), "age"]), collapse = ", ")), fill = TRUE)
+  }
+  
+}
+
+# define functions for time series data
+
+## Canadian data
+ts_canada <- function() {
+  
+  cat("\nTesting Canada time series...", fill = TRUE)
+  
+  for (type in types) {
+    
+    ### diff data
+    old_file <- paste0("old_", type, "_timeseries_canada")
+    new_file <- paste0(type, "_timeseries_canada")
+    var_names <- names(get(new_file))
+    date_var <- var_names[grepl("date", var_names)]
+    diff <- suppressMessages(compare_df(get(new_file), get(old_file), date_var))$comparison_df
+    
+    ### report differences
+    if (nrow(filter(diff, !!sym(date_var) == update_date & chng_type == "+")) == 0) {
+      cat(bgRed(paste0("Canada ", type, ": no update today?")), fill = TRUE)
+    } else if (nrow(diff) == 1 & nrow(filter(diff, !(!!sym(date_var) == update_date & chng_type == "+"))) == 0) {
+      # cat(green(paste0("Canada ", type, ": regular update.")), fill = TRUE) # don't report successes
+    } else {
+      diff <- filter(diff, !(!!sym(date_var) == update_date & chng_type == "+"))
+      cat(bgBlue(paste0("Canada ", type, ": regular update and historical modifications (", paste(unique(pull(diff, date_var)), collapse = ", "), ")")), fill = TRUE)
+    }
+    
+  }
+  
+}
+
+## provincial data
+ts_prov <- function() {
+  
+  cat("\nTesting provincial time series...\n", fill = TRUE)
+  
+  for (type in types) {
+  
+    ### diff data
+    old_file <- paste0("old_", type, "_timeseries_prov")
+    new_file <- paste0(type, "_timeseries_prov")
+    var_names <- names(get(new_file))
+    date_var <- var_names[grepl("date", var_names)]
+    diff <- suppressMessages(compare_df(get(new_file), get(old_file), date_var))$comparison_df
+    
+    ### loop through provinces
+    for (prov in unique(get(new_file)$province)) {
+      
+      ### report differences
+      diff_prov <- diff %>%
+        filter(province == prov)
+      if (nrow(filter(diff_prov, !!sym(date_var) == update_date & chng_type == "+")) == 0) {
+        cat(bgRed(paste0(prov, " ", type, ": no update today?")), fill = TRUE)
+      } else if (nrow(diff_prov) == 1 & nrow(filter(diff_prov, !(!!sym(date_var) == update_date & chng_type == "+"))) == 0) {
+        # cat(green(paste0(prov, " ", type, ": regular update.")), fill = TRUE) # don't report successes
+      } else {
+        diff_prov <- filter(diff_prov, !(!!sym(date_var) == update_date & chng_type == "+"))
+        cat(bgBlue(paste0(prov, " ", type, ": regular update and historical modifications (", paste(unique(pull(diff_prov, date_var)), collapse = ", "), ")")), fill = TRUE)
+      }
+      
+    }
+    
+  }
+  
+}
+
+## health region data
+### Interpretation note: If the script reports historical modifications for the entire time series back to the beginning,
+### it likely means the health region was newly added and the script has assembled a new time series
+### for the new health region going back to the beginning (filled with 0s except for the most recent date).
+### The new health region is either a misspelling of an existing health region or a "not reported" health region
+### for a province that did not previously have this.
+ts_hr <- function() {
+  
+  cat("\nTesting health region time series...\n", fill = TRUE)
+  
+  for (type in types_hr) {
+    
+    ### diff data
+    old_file <- paste0("old_", type, "_timeseries_hr")
+    new_file <- paste0(type, "_timeseries_hr")
+    var_names <- names(get(new_file))
+    date_var <- var_names[grepl("date", var_names)]
+    diff <- suppressMessages(compare_df(get(new_file), get(old_file), date_var))$comparison_df
+    hr_list <- distinct(select(diff, province, health_region)) # list health regions
+    
+    ### loop through provinces
+    for (prov in unique(get(new_file)$province)) {
+      
+      cat("Testing ", type, " from health regions in ", prov, "...\n", sep = "", fill = TRUE)
+      diff_prov <- diff %>%
+        filter(province == prov)
+      
+      ### loop through health regions
+      for (hr in hr_list %>% filter(province == prov) %>% pull(health_region)) {
+        
+        ### report differences
+        diff_hr <- diff_prov %>%
+          filter(health_region == hr)
+        if (nrow(filter(diff_hr, !!sym(date_var) == update_date & chng_type == "+")) == 0) {
+          cat(bgRed(paste0(hr, " (", prov, ") ", type, ": no update today?")), fill = TRUE)
+        } else if (nrow(diff_hr) == 1 & nrow(filter(diff_hr, !(!!sym(date_var) == update_date & chng_type == "+"))) == 0) {
+          # cat(green(paste0(hr, " (", prov, ") ", type, ": regular update.")), fill = TRUE) # don't report successes
+        } else {
+          diff_hr <- filter(diff_hr, !(!!sym(date_var) == update_date & chng_type == "+"))
+          cat(bgBlue(paste0(hr, " (", prov, ") ", type, ": regular update and historical modifications (", paste(unique(pull(diff_hr, date_var)), collapse = ", "), ")")), fill = TRUE)
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+}
+
+## print today's cumulative numbers and daily changes
+print_cumulative_today()
+
+## check ages in individual-level case data
+check_ages_cases()
+
+## check Canadian time series
+ts_canada()
+
+## check provincial time series
+ts_prov()
+
+## check health region time series
+ts_hr()
+
+# delete temporary files
+unlink(temp) # delete GitHub download
+unlink(paste(tempd, "Covid19Canada-master", sep = "/"), recursive = TRUE) # delete unzipped files
