a7fc374cb8e4be43a2e5c42f6134b27c2f3efcce
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-12-14 23:56:19 -0500

    Fix behaviour of run_at() when TZ is not set to ET.
    Previously, an improper TZ conversion would be performed which would result in undesirable behaviour on systems where the TZ was not already set to ET.

--- scripts/conductor_update_nightly.R
+++ scripts/conductor_update_nightly.R
@@ -31,11 +31,12 @@ get_time_et <- function() {
   with_tz(Sys.time(), tzone = "America/Toronto")
 }
 
-## run function after certain date & time (in ET time zone) has been reached
+## run function after certain date & time (assumes ET time zone) has been reached
 run_at <- function(time_et, FUN) {
-  cat("Wating until", time_et, "to continue...", fill = TRUE)
+  time_et <- as.POSIXct(time_et, tz = "America/Toronto") # assume ET time zone
+  cat("Waiting until", as.character(time_et), "to continue...", fill = TRUE)
   cat("Current time:", as.character(get_time_et()), fill = TRUE)
-  while(get_time_et() < with_tz(time_et, tzone = "America/Toronto")) {
+  while(get_time_et() < time_et) {
     cat(as.character(get_time_et()), "... waiting", fill = TRUE) # print current time
     Sys.sleep(30) # check every 30 seconds
   }
