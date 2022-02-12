f9d8bc378a376ce0550d0f53a083e4312c2958b7
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-02-12 16:11:24 -0500

    Update manual_update.R
    - Automatically copy old values for SK to current date

--- scripts/manual_update.R
+++ scripts/manual_update.R
@@ -59,3 +59,31 @@ swh_recovered() %>%
     col_names = FALSE,
     reformat = FALSE
   )
+
+# Update SK with old values
+update_sk <- function(sheet, read_cells, write_cells) {
+  # read cells
+  vals <- range_read(
+    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
+    sheet = sheet,
+    range = read_cells,
+    col_types = "c",
+    col_names = "val"
+  )
+  # write cells
+  range_write(
+    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
+    data = vals,
+    sheet = sheet,
+    range = write_cells,
+    col_names = FALSE,
+    reformat = FALSE
+  )
+}
+update_sk("cases_timeseries_hr", "F96:F102", "D96:D102")
+update_sk("mortality_timeseries_hr", "F96:F102", "D96:D102")
+update_sk("recovered_timeseries_prov", "E14", "C14")
+update_sk("testing_timeseries_prov", "E14", "C14")
+update_sk("vaccine_administration_timeseries_prov", "E13", "C13")
+update_sk("vaccine_completion_timeseries_prov", "E13", "C13")
+update_sk("vaccine_additional_doses_timeseries_prov", "E13", "C13")
