9008002d0be62c8e67957ecec6b65e35cf30ca70
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-02-17 16:09:21 -0500

    Update manual_update.R
    - copy_cells() can now read and write from different sheets
    - Add support for manual SK values from weekly epi report
    - Add support for manual NS values from daily news release

--- scripts/manual_update.R
+++ scripts/manual_update.R
@@ -29,11 +29,15 @@ library(reticulate)
 # source Python code
 source_python("scripts/manual_update.py")
 
-copy_cells <- function(sheet, read_cells, write_cells) {
+copy_cells <- function(read_sheet, read_cells, write_cells, write_sheet = NULL) {
+  # write sheet is same as read sheet
+  if (is.null(write_sheet)) {
+    write_sheet <- read_sheet
+  }
   # read cells
   vals <- range_read(
     ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
-    sheet = sheet,
+    sheet = read_sheet,
     range = read_cells,
     col_types = "c",
     col_names = "val"
@@ -42,7 +46,7 @@ copy_cells <- function(sheet, read_cells, write_cells) {
   range_write(
     ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
     data = vals,
-    sheet = sheet,
+    sheet = write_sheet,
     range = write_cells,
     col_names = FALSE,
     reformat = FALSE
@@ -100,13 +104,29 @@ swh_recovered() %>%
     reformat = FALSE
   )
 
+# Nova Scotia #
+copy_cells("manual_1", "A15:A19", "D33:D37", "cases_timeseries_hr")
+copy_cells("manual_1", "B15:B19", "D33:D37", "mortality_timeseries_hr")
+copy_cells("manual_1", "D19", "C7", "recovered_timeseries_prov")
+
 # Saskatchewan #
 
-# Update SK with old values
-copy_cells("cases_timeseries_hr", "F96:F102", "D96:D102")
-copy_cells("mortality_timeseries_hr", "F96:F102", "D96:D102")
-copy_cells("recovered_timeseries_prov", "E14", "C14")
-copy_cells("testing_timeseries_prov", "E14", "C14")
-copy_cells("vaccine_administration_timeseries_prov", "E13", "C13")
-copy_cells("vaccine_completion_timeseries_prov", "E13", "C13")
-copy_cells("vaccine_additional_doses_timeseries_prov", "E13", "C13")
+# Update SK with new values (Thursdays) or old values (other days)
+if (weekdays(date_today) == "Thursday") {
+  copy_cells("manual_1", "F29:F35", "D96:I102", "cases_timeseries_hr")
+  copy_cells("manual_1", "K29:K35", "D96:I102", "mortality_timeseries_hr")
+  # copy_cells("manual_1", "L30", "C14:N14", "recovered_timeseries_prov") # no longer provided
+  copy_cells("recovered_timeseries_prov", "E14", "C14") # just copy yesterday's value
+  copy_cells("manual_1", "M30", "C14:H14", "testing_timeseries_prov")
+  copy_cells("manual_1", "N30", "C13:H13", "vaccine_administration_timeseries_prov")
+  copy_cells("manual_1", "O30", "C13:H13", "vaccine_completion_timeseries_prov")
+  copy_cells("manual_1", "P30", "C13:H13", "vaccine_additional_doses_timeseries_prov")
+} else {
+  copy_cells("cases_timeseries_hr", "F96:F102", "D96:D102")
+  copy_cells("mortality_timeseries_hr", "F96:F102", "D96:D102")
+  copy_cells("recovered_timeseries_prov", "E14", "C14")
+  copy_cells("testing_timeseries_prov", "E14", "C14")
+  copy_cells("vaccine_administration_timeseries_prov", "E13", "C13")
+  copy_cells("vaccine_completion_timeseries_prov", "E13", "C13")
+  copy_cells("vaccine_additional_doses_timeseries_prov", "E13", "C13")
+}
