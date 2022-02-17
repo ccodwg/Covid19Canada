069e776de4b80ce2dc9a0c8ccdf9340a3f1ac383
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-02-17 14:13:12 -0500

    Update manual_update.R

--- scripts/manual_update.R
+++ scripts/manual_update.R
@@ -23,10 +23,50 @@ if (file.exists("/secrets.json")) {
 
 # load libraries
 library(magrittr)
+library(lubridate)
 library(reticulate)
 
 # source Python code
 source_python("scripts/manual_update.py")
+
+copy_cells <- function(sheet, read_cells, write_cells) {
+  # read cells
+  vals <- range_read(
+    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
+    sheet = sheet,
+    range = read_cells,
+    col_types = "c",
+    col_names = "val"
+  )
+  # write cells
+  range_write(
+    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
+    data = vals,
+    sheet = sheet,
+    range = write_cells,
+    col_names = FALSE,
+    reformat = FALSE
+  )
+}
+
+# set today's date
+date_today <- as.Date(with_tz(Sys.time(), "America/Toronto"))
+
+# Ontario PHUs #
+
+# Hastings Prince Edward (HPE)
+if (!weekdays(date_today) %in% c("Monday", "Wednesday", "Friday")) {
+  copy_cells("cases_timeseries_hr", "F50", "D50")
+  copy_cells("mortality_timeseries_hr", "F50", "D50")
+  copy_cells("recovered_timeseries_phu", "F12", "D12")
+}
+
+# Kingston Frontenac Lennox & Addington (KFL)
+if (!weekdays(date_today) %in% c("Monday", "Wednesday", "Friday")) {
+  copy_cells("cases_timeseries_hr", "F52", "D52")
+  copy_cells("mortality_timeseries_hr", "F52", "D52")
+  copy_cells("recovered_timeseries_phu", "F14", "D14")
+}
 
 # Southwestern (SWH)
 swh_cases() %>%
@@ -60,30 +100,13 @@ swh_recovered() %>%
     reformat = FALSE
   )
 
+# Saskatchewan #
+
 # Update SK with old values
-update_sk <- function(sheet, read_cells, write_cells) {
-  # read cells
-  vals <- range_read(
-    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
-    sheet = sheet,
-    range = read_cells,
-    col_types = "c",
-    col_names = "val"
-  )
-  # write cells
-  range_write(
-    ss = "1dTfl_3Zwf7HgRFfwqjsOlvHyDh-sCwgly2YDdHTKaSU",
-    data = vals,
-    sheet = sheet,
-    range = write_cells,
-    col_names = FALSE,
-    reformat = FALSE
-  )
-}
-update_sk("cases_timeseries_hr", "F96:F102", "D96:D102")
-update_sk("mortality_timeseries_hr", "F96:F102", "D96:D102")
-update_sk("recovered_timeseries_prov", "E14", "C14")
-update_sk("testing_timeseries_prov", "E14", "C14")
-update_sk("vaccine_administration_timeseries_prov", "E13", "C13")
-update_sk("vaccine_completion_timeseries_prov", "E13", "C13")
-update_sk("vaccine_additional_doses_timeseries_prov", "E13", "C13")
+copy_cells("cases_timeseries_hr", "F96:F102", "D96:D102")
+copy_cells("mortality_timeseries_hr", "F96:F102", "D96:D102")
+copy_cells("recovered_timeseries_prov", "E14", "C14")
+copy_cells("testing_timeseries_prov", "E14", "C14")
+copy_cells("vaccine_administration_timeseries_prov", "E13", "C13")
+copy_cells("vaccine_completion_timeseries_prov", "E13", "C13")
+copy_cells("vaccine_additional_doses_timeseries_prov", "E13", "C13")
