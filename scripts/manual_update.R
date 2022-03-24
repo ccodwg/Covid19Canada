b15f245ecd23a1356468df14aae553bf2814460d
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-03-24 16:11:24 -0400

    Update manual_update.R

--- scripts/manual_update.R
+++ scripts/manual_update.R
@@ -150,6 +150,9 @@ copy_cells("vaccine_administration_timeseries_prov", "E7", "C7")
 copy_cells("vaccine_completion_timeseries_prov", "E7", "C7")
 copy_cells("vaccine_additional_doses_timeseries_prov", "E7", "C7")
 
+# Alberta
+copy_cells("recovered_timeseries_prov", "E2", "C2")
+
 # British Columbia
 copy_cells("recovered_timeseries_prov", "E3", "C3") # recovered - copy yesterday's value
 
