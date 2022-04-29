885f26402131a535aa43f40cf021aeb96314f1f3
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-04-29 17:52:05 -0400

    Update manual_update.R

--- scripts/manual_update.R
+++ scripts/manual_update.R
@@ -12,13 +12,14 @@
 # You may be asked to give "Tidyverse API Packages" read/write access to your Google account.
 
 # authenticate with Google account (skip if already authenticated)
-if (!googlesheets4::gs4_has_token()) {
+library(googlesheets4)
+if (!gs4_has_token()) {
   if (file.exists("/secrets.json")) {
     # use service account key to authenticate non-interactively, if it exists
-    googlesheets4::gs4_auth(path = "/secrets.json")
+    gs4_auth(path = "/secrets.json")
   } else {
     # otherwise, prompt for authentication
-    googlesheets4::gs4_auth()
+    gs4_auth()
   }
 }
 
